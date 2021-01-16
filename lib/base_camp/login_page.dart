c3809d0ddff690a7c9f4c04bb0a77438390d15a2
Dimstein <dimstein.nz@gmail.com>
2021-01-16 19:13:40 +1300

    login screen initital

---
+++ lib/base_camp/login_page.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+
+  TextEditingController emailTEC, passwordTEC;
+
+  @override
+  void dispose() {
+    emailTEC.dispose();
+    passwordTEC.dispose();
+    super.dispose();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(Icons.lock_sharp),
+      title: Text('Field Notes Login'),
+        actions: [
+          Icon(Icons.logout)
+        ],
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            TextFormField(
+              controller: emailTEC,
+              decoration: InputDecoration(
+                icon: Icon(Icons.person),
+                hintText: 'Login Email',
+                labelText: 'email'
+              ),
+              onSaved: (String value){},
+              validator: (String value){
+                return value.contains('@') ? 'Not a email address' : null;
+              },
+
+            ),
+            TextFormField(
+              controller: passwordTEC,
+                decoration: InputDecoration(
+                    icon: Icon(Icons.cake),
+                    hintText: 'Login Password',
+                    labelText: 'password'
+                )
+            ),
+            RaisedButton.icon(
+                onPressed: (){},
+                icon: Icon(Icons.login_rounded),
+                label: Text('Login')),
+
+          ],
+        )
+        ,
+      ),
+    );
+  }
+}
